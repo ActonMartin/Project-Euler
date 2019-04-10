d7aaf414aa12862cc434554cb9f3ca53842d3317
ActonMartin <wszcla@outlook.com>
2019-04-10 17:01:29 +0800

    序列中的最大数

--- 8.Largest product in a series.py
+++ 8.Largest product in a series.py
@@ -1,3 +1,7 @@
+'''
+The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+'''
 num = '''73167176531330624919225119674426574742355349194934
 96983520312774506326239578318016984801869478851843
 85861560789112949495459501737958331952853208805511
